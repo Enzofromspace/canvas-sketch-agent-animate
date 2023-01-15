ab6f6e515f0d6d4c9a535ae3fa55806c50e623a4
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2023-01-15 13:03:00 +0300

    Drew a single dot

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+
+    const point = {x: 800, y: 400};
+
+    context.beginPath();
+    context.arc(point.x, point.y, 10, 0, Math.PI * 2);
+    context.fillStyle = 'black';
+    context.fill();
+
   };
 };
 
