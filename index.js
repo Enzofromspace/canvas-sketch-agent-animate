9249ccd4843f91831b7c358086421e55b2c37e40
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2023-01-15 13:36:45 +0300

    added loop, classes to draw 40 dots at random

--- index.js
+++ index.js
@@ -1,22 +1,49 @@
 const canvasSketch = require('canvas-sketch');
+const random = require('canvas-sketch-util/random');
 
 const settings = {
   dimensions: [1080, 1080],
 };
 
-const sketch = () => {
+const sketch = ({ width, height }) => {
+  const agents = [];
+  
+  for(let i = 0; i < 40; i++){
+    const x = random.range(0, width);
+    const y = random.range(0, height);
+
+    agents.push(new Agent(x,y));
+  }
+
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    const point = {x: 800, y: 400};
-
-    context.beginPath();
-    context.arc(point.x, point.y, 10, 0, Math.PI * 2);
-    context.fillStyle = 'black';
-    context.fill();
-
+    agents.forEach(agent => {
+        agent.draw(context);
+    });
   };
 };
 
 canvasSketch(sketch, settings);
+
+//create a class and constructor for new points on canvas
+class Point {
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+  }
+}
+
+class Agent {
+  constructor(x, y) {
+    this.pos = new Point(x, y);
+    this.radius = 10;
+  }
+  draw(context){
+    context.fillStyle = 'black';
+    context.beginPath();
+    context.arc(this.pos.x, this.pos.y, this.radius, 0, Math.PI * 2);
+    context.fill();
+  }
+}
